0d02f7695aa6ccc909d92a72915bfde702b28595
sojung32 <sojung0320@gmail.com>
2023-03-26 22:11:27 +0900

    Add AntDesign

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 
 import { Button } from './Button';
 
+/**
+ * 2뎁스 컴포넌트
+ * export default {
+ *  title: '1뎁스제목/2뎁스제목',
+ *  component: 컴포넌트명,
+ *  argTypes: {
+ *    스타일, 함수 등
+ *  },
+ * };
+ */
 export default {
   title: 'Example/Button',
   component: Button,
@@ -12,6 +22,13 @@ export default {
 
 const Template = (args) => <Button {...args} />;
 
+/**
+ * 3뎁스 컴포넌트
+ * export const 옵션명 = Template.bind({});
+ * 옵션명.args = {
+ *  컨트롤 옵션
+ * };
+ */
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
---
+++ src/stories/Calendar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Calendar } from 'antd';
+
+export const CalendarAntD = ({...props }) => {
+  return (
+    <>
+      <Calendar />
+    </>
+  );
+};
